820d641123bf266d6a12309bc31a461a6a8736c6
danbretl <danbretl@gmail.com>
2012-09-24 13:44:15 -0500

    Initial Commit

---
+++ PhotoFlow/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PhotoFlow
+//
+//  Created by Dan Bretl on 9/24/12.
+//  Copyright (c) 2012 Dan Bretl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "PFAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([PFAppDelegate class]));
+    }
+}
